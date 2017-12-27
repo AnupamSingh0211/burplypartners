570f905fca5e524f1ace2daad429a3a33dbf9812
anupamsingh <anupamsingh@urbanclap.com>
2017-12-28 00:40:30 +0530

    added onboarding activity

--- app/src/main/java/com/hala/burplypartners/OnboardingActivity.kt
+++ app/src/main/java/com/hala/burplypartners/OnboardingActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity() {
 
     private lateinit var viewPager: ViewPager
     private lateinit var pagerAdapter: OnboardingPagerAdapter
